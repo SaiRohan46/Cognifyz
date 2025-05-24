ff0402ad5f54859cbea3349def3890276cec72fb
SaiRohan46 <rohansai4645@gmail.com>
2025-05-24 11:44:50 +0530

    Create script.js

---
+++ task-4/script.js
@@ -0,0 +1,5 @@
+function changeBackground(){
+    const colors=["#f6d365","#fda085","#a1c4fd","#c2e9fb","#d4fc79"];
+    document.body.style.backgroundColor=colors[Math.floor(Math.random()*colors.length)];
+  }
+  
