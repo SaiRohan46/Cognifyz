d5ea3b47a4001f375dbe1c19a9d5763b2a7de795
SaiRohan46 <rohansai4645@gmail.com>
2025-05-24 11:48:27 +0530

    Add files via upload

---
+++ Task-8/script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    const messageBox = document.getElementById("messageBox");
+    messageBox.style.display = "block";
+    messageBox.innerHTML = "Thank you for learning more about Sass styling!";
+  }
+  
